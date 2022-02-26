c0171ea3e390ce0d4d48193550ec46a84c429d62
Vcvc <vcvckw@gmail.com>
2022-02-27 02:51:59 +0800

    init

--- chrome_selinum.py
+++ chrome_selinum.py
@@ -1,71 +1,17 @@
-import random
 import os
 import time
-import sys
-import datetime
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from config import m_con
+from selenium.webdriver.common.by import By
+from config import m_con, get_random_useragent
+from datetime import datetime
+
 
 
 """
-https://zhuanlan.zhihu.com/p/111859925
-https://zhuanlan.zhihu.com/p/60852696
-https://blog.csdn.net/dragongreenfire/article/details/114915966
+https://h5.stararknft.art/#/newBlindBox?id=10675&isbox=1
 """
-
-USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
-    "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
-    "Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.90 Safari/537.36",
-    "Mozilla/5.0 (X11; NetBSD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
-    "Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.14 (KHTML, like Gecko) Chrome/24.0.1292.0 Safari/537.14"
-    ]
-
-def get_random_useragent():
-    """生成随机的UserAgent
-    :return: UserAgent字符串
-    """
-    return random.choice(USER_AGENTS)
 
 if __name__ == '__main__':
     print('Start')
@@ -75,6 +21,8 @@ if __name__ == '__main__':
 
     # 打开网页
     chrome_ua = get_random_useragent()
+
+    print(m_con.buytarget)
     moptions.add_argument(chrome_ua)
     moptions.add_argument('--disable-gpu')
     moptions.add_argument('blink-settings=imagesEnabled=false') #禁用图片
@@ -85,24 +33,46 @@ if __name__ == '__main__':
     #options.add_argument('--headless')
 
     # 禁用浏览器弹窗
-    prefs = {
-        'profile.default_content_setting_values' :  {
-            'notifications' : 2
-         }
-    }
-
-    moptions.add_experimental_option('prefs',prefs)
+    # prefs = {
+    #     'profile.default_content_setting_values' :  {
+    #         'notifications' : 2
+    #      }
+    # }
+    # moptions.add_experimental_option('prefs',prefs)
 
     browser = webdriver.Chrome(options=moptions)
+    """
+    ----------------------------------------------
+    """
 
 
-    browser.get('https://h5.stararknft.art/#/login')
-    time.sleep(10)
-    browser.get('https://h5.stararknft.art/#/newBlindBox?id=10675&isbox=1')
-    time.sleep(0.1)
+    browser.get(m_con.loginurl) #未登录自动跳转到登录
+    time.sleep(2)
+    browser.refresh()  # 刷新方法 refresh
+    browser.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div[1]/input").send_keys(m_con.username)
+    browser.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div[2]/input").send_keys(m_con.password)
+    time.sleep(2)
+    browser.find_element(By.XPATH, "/html/body/div/div[1]/span").click() #登录成功
+    time.sleep(2)
 
-    username = '13186328632'
-    password = 'kewang@456'
+    start_time = datetime.strptime('2022-02-27 19:59:59.850', "%Y-%m-%d %H:%M:%S.%f")
+    now_time = datetime.now
 
-
-
+    while True:
+        if now_time() >= start_time:
+            try:
+                browser.get(m_con.buytarget)
+                time.sleep(0.2)
+                browser.find_element(By.XPATH, "/html/body/div/div[1]/div[5]/button").click() #购买按钮
+                time.sleep(0.2)
+                browser.find_element(By.XPATH, "/html/body/div/div[1]/div[6]/div[2]/div/span").click() #购买按钮2
+                time.sleep(0.2)
+                browser.find_element(By.XPATH, "/html/body/div/div[1]/div[6]/div[4]/div/div[1]/input").send_keys(m_con.payword)
+                time.sleep(0.1)
+                browser.find_element(By.XPATH, "/html/body/div/div[1]/div[6]/div[4]/div/div[2]/span[2]").click() #购买按钮2
+            except:
+                pass
+            break
+        else:
+            time.sleep(0.1)
+            print('sleep 0.1')
