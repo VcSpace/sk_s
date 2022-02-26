4ceccdb3f0c6d74aad04d62e066421d662b72ddf
Vcvc <vcvckw@gmail.com>
2022-02-26 23:50:38 +0800

    con

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+import time
+
+class Config(object):
+    def __init__(self):
+       self.username = '13186328632'
+       self.password = 'kewang@456'
+
+m_con = Config()
